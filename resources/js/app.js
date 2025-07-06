a5c8bb9cae414441c4bb3ab963046f8be054662e
GuSt4v0CCAM4 <alonsospinola70@gmail.com>
2025-07-06 00:34:33 -0500

    fix navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import {createInertiaApp} from "@inertiajs/vue3"; // Import Inertia
 import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers"; // Import Inertia helpers
 import {Head, Link} from "@inertiajs/vue3"; // Import Inertia components
 
+import { defineAsyncComponent } from 'vue'; // Import defineAsyncComponent for lazy loading
+
 //importarmos el css
 import '../css/app.css';
 
@@ -17,8 +19,10 @@ createInertiaApp({
             .use(plugin)
             .component('Head', Head) // Register Inertia Head component
             .component('Link', Link) // Register Inertia Link component
-        //Registramos componente globales (opcional)
-        app.component('Navbar', () => import('./Components/Layout/Navbar.vue'));
+            // Registramos componente globales (opcional)
+            .component('Navbar', defineAsyncComponent(() =>
+            import('./Components/Layout/Navbar.vue')
+            ));
         app.mount(el);
     },
 });
